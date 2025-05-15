dbdbc6c5dfb7b47e2ec0e065ad0aca725659cbbf
hadi alghoul <hadialghoul2003@gmail.com>
2025-05-15 16:07:10 +0300

    Initial commit from Create Next App

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+
+import React from 'react'
+import Image from 'next/image'
+
+export default function Navbar() {
+  return (
+    <div>
+        <div>
+            <Image src={} height={40} width={40} />
+            <span>Crady</span>
+        </div>
+        <div>
+            <Link  href="">Features</Link>
+            <Link  href="">How It Works</Link>
+            <Link  href="">Downloads</Link>
+        </div>
+        <div>
+            <a>
+                
+            </a>
+        </div>
+    </div>
+  )
+}
+
